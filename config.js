3ffa4fd80bde076f49b1c565b9863a65c739d642
sologash <51867302+sologash@users.noreply.github.com>
2023-04-25 19:22:18 -0500

    update api

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
-export const DEEPL_API_KEY = 'TU_CLAVE_API_AQUI';
+const config = {
+    	apiKey: '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',
+        	modelId: 'ChatGPT-es-2022-04-25',
+            };
+
+}
